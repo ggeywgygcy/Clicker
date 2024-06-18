d5d6e925cc82cd8669ad4c0384869695a6414ef5
ggeywgygcy <wasilij18bus@gmail.com>
2024-06-18 12:36:51 +0300

    кликер получился, в будущем надо доработать!

--- clicker.js
+++ clicker.js
@@ -1,8 +1,11 @@
 const inputEl = document.getElementById('exampleInputEmail1')
 const clickBtn = document.getElementById('btn')
-const vivEl = document.getElementById('viv')
+// const vivEl = document.getElementById('viv')
 
 clickBtn.onclick = function(res){
-     res = inputEl.value + 1
-     vivEl.textContent = res
+    res = inputEl.value++
+     
+    // if (inputEl.val === 1000){
+    //     vivEl.textContent == "Нифига ты автобот"
+    // }
 }
